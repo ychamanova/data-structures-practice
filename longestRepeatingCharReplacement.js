5a38302c39f30101caa674df5f5e0bac145d8450
Yelena Chamanova <ychamanova@gmail.com>
2021-03-22 10:41:03 -0700

    longest char repl

---
+++ longestRepeatingCharReplacement.js
@@ -0,0 +1,30 @@
+/*
+Given a string s that consists of only uppercase English letters, you can perform at most k operations on that string.
+
+In one operation, you can choose any character of the string and change it to any other uppercase English character.
+
+Find the length of the longest sub-string containing all repeating letters you can get after performing the above operations.
+*/
+
+var characterReplacement = function (s, k) {
+  let left = 0;
+  let right = 0;
+  let maxCharCount = 0;
+  const visited = {};
+
+  while (right < s.length) {
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
+
+    if (visited[char] > maxCharCount) maxCharCount = visited[char];
+
+    if (right - left + 1 - maxCharCount > k) {
+      visited[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+
+  return right - left;
+};
